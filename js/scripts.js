a3ba108e5f5fd221bb016b07413a546a7e0ed0b6
hfultondesign <hfultondesign@gmail.com>
2022-10-24 13:15:58 -0400

    2.3 - Added JS Menu toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
 console.log("Hi, welcome to my portfolio!")
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+if (x.className === 'navtoggle') {
+  x.className += ' responsive'; // changes class of object based on whether it's already shown or not (if menu open, click will close, vice versa)
+  } else {
+    x.className = 'navtoggle';
+      }
+}
