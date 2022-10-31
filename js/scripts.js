7af727def2dd68a0ac919b552ebbb23ed30d1941
hfultondesign <hfultondesign@gmail.com>
2022-10-31 12:56:05 -0400

    2.5 - Code clean up

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,16 @@ if (x.className === 'navtoggle') {
     x.className = 'navtoggle';
       }
 }
+function menuToggle(x) {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
+
+$('#click_advance').click(function() {
+    $('#display_advance').toggle('1000');
+    $("i", this).toggleClass("fas fa-bars fas fa-x");
+});
